874a28d986293f931eabea00e311f765701a2688
Vignesh N <iam@vigneshn.in>
2022-10-16 11:53:00 +0530

    changed runtime and config

--- config/website.js
+++ config/website.js
@@ -1,8 +1,8 @@
 module.exports = {
-  siteTitle: "Lanyard", // Navigation and Site Title
-  siteTitleShort: "Lanyard", // short_name for manifest
+  siteTitle: "ACD2022-Lanyard", // Navigation and Site Title
+  siteTitleShort: "ACD2022", // short_name for manifest
   siteLogo: "images/logo.png", // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: "The Agenda PWA for your Conference.",
+  siteDescription: "The Agenda PWA for ACD2022.",
 
   // Manifest and Progress color
   themeColor: "#1E2A39", //
